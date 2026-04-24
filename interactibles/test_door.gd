f32f8a192cca23c83aa48c19f324b8d09c5db83c
zTowerDive <MetallicGreyYT@gmail.com>
2026-04-23 21:27:03 -0400

    Added temporary testing code to TESTDoor
    TESTDoor can be opened by pressing the interact key
    
    TESTDoor properly animated when interacted with

--- interactibles/test_door.gd
+++ interactibles/test_door.gd
@@ -14,7 +14,22 @@ func interact() -> void:
 	set_is_active(not is_active)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		interact()
+
 
 func set_is_active(new_value: bool) -> void:
 	is_active = new_value
 	_static_body_collision_shape.disabled = is_active
+	
+	
+	var top_value := 2.45 if is_active else 0.5
+	
+	if _tween_door != null:
+		_tween_door.kill()
+	
+	_tween_door = create_tween().set_ease(Tween.EASE_OUT)
+	_tween_door.set_trans(Tween.TRANS_BACK if is_active else Tween.TRANS_BOUNCE)
+	
+	_tween_door.tween_property(_door_mesh, "position:y", top_value, 1.0)
